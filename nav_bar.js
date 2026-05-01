35302073a7bf9341702d8b781a01b4d8e16f2081
xiuyuan1214-debug <xiuyuan1214@gmail.com>
2026-05-01 22:19:35 +0800

    Update nav_bar.js

--- nav_bar.js
+++ nav_bar.js
@@ -3,11 +3,11 @@
         <div class="container">
             <a href="index.html" class="logo">跑班訓練營</a>
             <ul>
-                <li><a href="index.html">首頁</a></li>
                 <li><a href="about.html">教練資歷</a></li>
                 <li><a href="courses.html">課程簡介</a></li>
                 <li><a href="tools.html">配速工具</a></li>
-                <li><a href="testimonials.html">學員見證</a></li>
+                <li><a href="training.html">訓練日誌</a></li>
+                <li><a href="match.html">賽事日誌</a></li>
               </ul>
         </div>
     </nav>`;
