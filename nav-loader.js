92e90e18fb101cfabf1910d42873e60bd0435ce4
xiuyuan1214-debug <xiuyuan1214@gmail.com>
2026-05-04 14:56:00 +0800

    Update nav-loader.js

--- nav-loader.js
+++ nav-loader.js
@@ -6,7 +6,7 @@ function loadNav() {
             <a href="index.html" class="logo">跑班訓練營</a>
             <ul>
                 <li><a href="index.html">首頁</a></li>
-                <li><a href="about.html">教練資歷</a></li>
+                <li><a href="index.html">教練資歷</a></li>
                 <li><a href="courses.html">課程簡介</a></li>
                 <li><a href="tools.html">配速工具</a></li>
                 <li><a href="testimonials.html">學員見證</a></li>
