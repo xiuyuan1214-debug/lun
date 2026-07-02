213f0057551752edaf01bbe3e2895d6fda9b6073
xiuyuan1214-debug <xiuyuan1214@gmail.com>
2026-07-02 13:09:37 +0800

    Update nav_bar.js

--- nav_bar.js
+++ nav_bar.js
@@ -3,7 +3,7 @@ document.getElementById('navBar').innerHTML = `
     <a href="about.html" class="nav-item">教練資歷</a>
     <a href="courses.html" class="nav-item">課程簡介</a>
     <a href="nav_bar.js.html" class="nav-item">配速工具</a>
-    <a href="training.html" class="nav-item">訓練日誌</a>
+    <a href="training.html" class="nav-item">品牌故事</a>
     <a href="match.html" class="nav-item">賽事日誌</a>
     <a href="form.html" class="nav-item">聯絡我們</a>
 </div>
